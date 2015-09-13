db7e2a4ec32d7d13125dbec257a204a1f98caf16
Corbin Halliwill <corbin.halliwill@berkeley.edu>
2015-09-13 15:50:11 -0700

    add validation on trip properties

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,12 @@
 class Trip < ActiveRecord::Base
+  validates_presence_of :user_id
+  validates_presence_of :start_latitude
+  validates_presence_of :start_longitude
+  validates_presence_of :end_latitude
+  validates_presence_of :end_longitude
+  validates_presence_of :end_time
+  validates_presence_of :group_ids
+
   belongs_to :users
 
   has_and_belongs_to_many :groups
