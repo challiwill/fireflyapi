fa1ccf08b6d4e0f2459995bdde24e2dc1fda8616
Naseem Alnaji <nalnaji@berkeley.edu>
2015-09-16 10:45:25 -0700

    Added styling to how our RSPEC outputs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,6 +31,13 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  # Colors output to be Green/Yellow/Red
+  config.color = true
+  config.tty = true
+
+  # Shows the actual tests when they are run
+  config.formatter = :documentation
+
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
